084032c00f33cc37f4bf57a91f20fae528fb1201
Muntasir Adnan <adnaan525@gmail.com>
2019-05-07 23:06:34 +1000

    Job Modified, Server Modified. One server class will be used to keep track and a server list : listServer

--- Server.java
+++ Server.java
@@ -1,8 +1,36 @@
 
 public class Server{
+	String serverType;
 	int serverID;
-    int remaining;
-    int remCore;
-    int remMem;
-    int remDisk;
+	int state; 	//Inactive = 0, 
+				//Booting = 1, 
+				//Idle = 2, 
+				//Active = 3, 
+				//Unavailable = 4
+    int avbTime;
+    int core;
+    int memory;
+    int dsk;
+    
+    public Server()
+    {
+    	
+    }
+    
+    public Server(String[] arr)
+    {
+    	if (arr.length != 7)
+    	{
+    		System.out.println("Server can not be added. Insufficient value received");
+    		System.out.println("--------------------------------------------------");
+    	}
+    	this.serverType = arr[0];
+    	this.serverID = Integer.parseInt(arr[1]);
+    	this.state = Integer.parseInt(arr[2]);
+    	this.avbTime = Integer.parseInt(arr[3]);
+    	this.core = Integer.parseInt(arr[4]);
+    	this.memory = Integer.parseInt(arr[5]);
+    	this.dsk = Integer.parseInt(arr[6]);
+    } 
 }
+
