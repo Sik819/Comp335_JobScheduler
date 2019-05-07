1ff84b567a6f6ceda77570965d572cefc7c46b2b
Muntasir Adnan <adnaan525@gmail.com>
2019-05-07 23:19:41 +1000

    **Start from here, Stage 1 working** OKsender, add job changed. Using string split. Added 2 lists to keep track of servers and jobs. Sorting added to okSender. Main can handle arguments.

--- Client.java
+++ Client.java
@@ -9,20 +9,24 @@ import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Scanner;
-
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.xml.sax.SAXException;
-
 import java.io.BufferedReader;
-
-public class Client extends Job {
-    ArrayList<Job> listJOB = new ArrayList<>();
+import java.util.*;
+public class Client {
+	Socket socket =  null;
+	
+	//job
+    ArrayList<Job> listJob = new ArrayList<>();
     Job currentJob = new Job();
-    static Parser p = new Parser();
-    Socket socket =  null;
+    
+    //server
+    ArrayList<Server> listServer = new ArrayList<>();
+    HashMap<String,Server[]> newListServer = new HashMap<>();
+    
+    //get the largest server
+    public String getLargest()
+    {
+    	return listServer.get(listServer.size()-1).serverType;
+    }
 
     //make byte array
     public byte[] sendToServer(String s)
@@ -32,78 +36,92 @@ public class Client extends Job {
         return bytes;
 
     }
-    //read line
+    
+    //reads a message from the server and returns the message as a string
     public String readLine(Socket s) throws UnsupportedEncodingException, IOException
     {
         Job j = new Job();
         BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream(), "UTF-8"));//receive buffer
         String line = input.readLine();
-        System.out.println("Received "+line);
 
         if(line.contains(("JOBN"))) {
-            seperateStrings(j, line);
-            listJOB.add(j);
-            currentJob = j;
+            addJob(line);
+            System.out.println(currentJob.toString());
         }
-
-        //		if(line.contains("JOBN"))
-//		{
-//			int length = 0;
-//			int begin = 0;
-//			while(length <= line.length())
-//			{
-//				char ch = line.charAt(length);
-//				if(ch == ' ')
-//				{
-//					j.jobAl.add(line.substring(begin, length));
-//				}
-//				length++;
-//				length = begin;
-//			}
-//			return j;
-//		}
         return line;
     }
     
     //schedule the job
-    public void scheduleJob(PrintStream pr, Socket s, Job j, Parser par) throws UnsupportedEncodingException, IOException
+    public void scheduleJob(PrintStream pr, Socket s, Integer useAlg) throws UnsupportedEncodingException, IOException //add parser par
     {
-    	pr.write(sendToServer("SCHD "+j.get(2)+" "+par.listServers.getLargest()+" 0"));
+    	String str = null;
+    	if (useAlg == null || useAlg != 1) //null or not ff
+    		str = listServer.get(listServer.size()-1).serverType+" 0"; //scheduling to the largest
+    	else if (useAlg == 1)
+    		str = "";
+    	pr.write(sendToServer("SCHD "+currentJob.jobID+" "+str));
+    	System.out.println(">>> "+currentJob.jobID+" SCHEDULED TO :"+str);
         if (readLine(s).contains("OK"))
         {
-            j.jobDone();
+            currentJob.jobDone();
+            listServer.clear();
         }
-    }
-    //getting job IDs
-    public Job seperateStrings(Job j , String s){
-        if(s.length()<=1)
-            return j;
-        if(!s.contains(" ")) {
-            j.add(s);
-            return j;
-        }
-        j.add(s.substring(0,s.indexOf(' ')));
-        return  seperateStrings(j,s.substring(s.indexOf(' ')+1));
     }
     
-    //send ok until .
-    public void okSender(PrintStream send, String reply) throws IOException {
+    //getting job IDs
+    public void addJob(String str){
+    	String[] jobStr = str.split(" ");
+    	Job job = new Job(jobStr);
+    	listJob.add(job);
+    	currentJob = job;
+    			
+    	
+//        if(s.length()<=1)
+//            return j;
+//        if(!s.contains(" ")) {
+//            j.add(s);
+//            return j;
+//        }
+//        j.add(s.substring(0,s.indexOf(' ')));
+//        return  seperateStrings(j,s.substring(s.indexOf(' ')+1));
+    	
+    }
+    
+    //send OK until "."
+    public void okSender(PrintStream send) throws IOException {
     	while(true)
         {
-            send.write(sendToServer("OK"));
-            reply = readLine(socket);
-            if(reply.equals("."))
+        	String serverString = readLine(socket); //get server info or "DATA"
+        	//System.out.println(serverString);
+        	if(serverString.contains("DATA"))
+        	{
+        		send.write(sendToServer("OK"));
+        	}
+        	else if(serverString.equals("."))
             {
+        		Collections.sort(listServer, new CompareByCore());
                 break;
             }
+        	else
+        	{
+            	String[] serverDetails = serverString.split(" ");
+                Server server = new Server(serverDetails);
+                listServer.add(server);
+                send.write(sendToServer("OK"));
+        	}
         }
     	
     }
 
-    //constructor
-    public Client(String ip, int port) throws UnknownHostException, IOException
+    //constructor client
+    
+    public Client(String ip, int port, Integer algNumber) throws UnknownHostException, IOException
     {
         socket = new Socket(ip, port);
+        System.out.println("Connected with server");
+        System.out.println("-----------------------------");
+        System.out.println();
+        
         //BufferedReader rcv = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintStream send = new PrintStream(socket.getOutputStream()); //send message
 
@@ -115,40 +133,32 @@ public class Client extends Job {
         for(String s : al)
         {
             send.write(sendToServer(s));
-            System.out.println("Sending "+s);
             String reply = readLine(socket);
-            if(s.equalsIgnoreCase("AUTH USER") && reply.equalsIgnoreCase(("OK"))) {
-                try {
-                    p.parse();
-                } catch (ParserConfigurationException | SAXException e) {
-                    e.printStackTrace();
-                }
-            }
         }
-        send.write(sendToServer("RESC All"));
-        String reply = readLine(socket); //get server info
-        okSender(send, reply);  //get all server info
-        System.out.println("Job(s) :"+currentJob);
+
+        send.write(sendToServer("RESC Avail "+currentJob.getJobRESC()));
+        okSender(send);  //get all servers
+        
         //do the scheduling
-        send.write(sendToServer("SCHD "+currentJob.get(2)+" "+p.listServers.getLargest()+" 0"));
-        //scheduleJob(send, socket, currentJob, p);
+        scheduleJob(send, socket, algNumber);
         while(true)
         {
         	send.write(sendToServer("REDY"));
         	String str = readLine(socket);
         	if(str.contains("JOBN"))
         	{
-        		send.write(sendToServer("RESC All"));
-        		readLine(socket);
-        		okSender(send, reply);
-        		scheduleJob( send, socket, currentJob, p);
+        		send.write(sendToServer("RESC Avail "+currentJob.getJobRESC()));
+        		okSender(send);
+        		scheduleJob(send, socket, algNumber);
         	}
-        	if(str.equals("NONE"))
+        	else if(str.equals("NONE"))
         	{
         		break;
         	}
         }
         send.write(sendToServer("QUIT"));
+        
+        System.out.println();
 		System.out.println("Connection closing");
 		System.out.println("-----------------------------");
         socket.close();
@@ -156,11 +166,20 @@ public class Client extends Job {
         
 }
     public static void main(String[] args) throws UnknownHostException, IOException {
-        if(args.length!=1)
-            throw new RuntimeException("Enter file path of your server");
-        else
-            p = new Parser(args);
-        Client cl = new Client("127.0.0.1", 8096);
+    	//alg values
+    	//	ff = 1
+    	//	bf = 2
+    	//	wf = 3
+    	
+    	Integer algorithm = null;
+    	if (args.length != 0)
+    	{
+    		if(args[0].contains("-a") && args[1].contains("ff"))
+        		algorithm = 1;
+    	}
+    	
+
+        Client cl = new Client("127.0.0.1", 8096, algorithm);
 
     }
 }
